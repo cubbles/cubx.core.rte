bc8eda388a8a0a44d83e0791a100e36a82eeedb8
Judit Ross <jtrs@incowia.com>
2016-05-13 14:41:31 +0200

    PLAT-497:  wip

--- devtools-mappedIn/lib/detect-workspace.js
+++ devtools-mappedIn/lib/detect-workspace.js
@@ -1,6 +1,7 @@
 'use strict';
+var path = require('path');
 module.exports = function (grunt, workspaceName) {
-  var defaultWorkspacePath = '../' + workspaceName + '/';
+  var defaultWorkspacePath = path.join('..', workspaceName);
   var alternativeWorkspacePath = './' + workspaceName + '/';
 
   if (grunt.file.isDir(defaultWorkspacePath)) {
@@ -8,6 +9,7 @@ module.exports = function (grunt, workspaceName) {
   } else if (grunt.file.isDir(alternativeWorkspacePath)) {
     return alternativeWorkspacePath;
   }
+  console.log('defaultWorkspacePath', defaultWorkspacePath);
   // also if it does not exist, return the default path
   return defaultWorkspacePath;
 };
