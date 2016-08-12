aa37fed5953cdf670ed18fa4d86f35080677bbbb
Philipp Wagner <philipp.wagner@incowia.com>
2016-08-12 16:01:57 +0200

    PLAT-553: add test data, add first konverter methods (WIP)

--- webpackages/cubx.core.rte/crc/modules/manifestConverter/manifestConverter.js
+++ webpackages/cubx.core.rte/crc/modules/manifestConverter/manifestConverter.js
@@ -5,13 +5,69 @@ window.cubx.amd.define([], function () {
   'use strict';
 
   /**
-   * The manifestConverter can be used to convert manifest.webpackage objects between major model Versions.
+   * The manifestConverter can be used to convert manifest.webpackage objects between model Versions.
    * @class ManifestConverter
    * @global
    * @constructor
    */
   var ManifestConverter = function () {
-    console.log('manifestConverter initialized...');
+    /**
+     * Holds a well defined transformation matrix defining all transformations that need to be done depending on source
+     * ModelVersion. Each item contains all transformations that need to be done to convert to current given _targetVersion.
+     * @type {object}
+     * @private
+     */
+    this._transformationMatrix = {
+      '8.x.x': [
+        this._addResourcesArrayToArtifacts
+      ],
+      '9.0.0': [],
+      '9.1.0': []
+    };
+
+    /**
+     * Define target model version for output manifest files
+     * @type {string}
+     * @private
+     */
+    this._targetVersion = '9.1.0';
+  };
+
+  /**
+   * Based on given _transformationMatrix determine the transformations that need to be proceeded
+   * @memberOf ManifestConverter
+   * @param {string} sourceModelVersion The source modelVersion
+   * @return {array} An array of methods to call in provided order
+   * @private
+   */
+  ManifestConverter.prototype._determineTransformationList = function (sourceModelVersion) {
+    var transformationList = [];
+
+    if (sourceModelVersion.indexOf('8.') === 0) {
+      // deal with modelVersion 8
+      transformationList = this._transformationMatrix['8.x.x'];
+    } else if (this._transformationMatrix.hasOwnProperty(sourceModelVersion)) {
+      transformationList = this._transformationMatrix[sourceModelVersion];
+    }
+
+    return transformationList;
+  };
+
+  ManifestConverter.prototype._addResourcesArrayToArtifacts = function (manifest) {
+
+  };
+
+  /**
+   * Convert a given manifest files into latest modelVersion
+   * @param {object} manifest
+   * @return {object}convertedManifest
+   * @memberOf ManifestConverter
+   */
+  ManifestConverter.prototype.convert = function (manifest) {
+    // var modelVersion = manifest.modelVersion;
+    var convertedManifest = JSON.parse(JSON.stringify(manifest));
+
+    return convertedManifest;
   };
 
   return new ManifestConverter();
--- webpackages/cubx.core.rte/crc/test/unit/manifestConverter_test.js
+++ webpackages/cubx.core.rte/crc/test/unit/manifestConverter_test.js
@@ -1,8 +1,15 @@
-window.cubx.amd.define(['manifestConverter'], function (manifestConverter) {
-  'use strict';
+window.cubx.amd.define(
+  [
+    'manifestConverter',
+    'text!unit/manifestConverter/manifest@8.3.1.json',
+    'text!unit/manifestConverter/manifest@9.0.0.json',
+    'text!unit/manifestConverter/manifest@9.1.0.json'
+  ],
+  function (manifestConverter, manifest831, manifest900, manifest910) {
+    'use strict';
 
-  describe('ManifestConverter', function () {
-    console.log('Running ManifestConverter Unit Tests...');
+    describe('ManifestConverter', function () {
+      console.log('Running ManifestConverter Unit Tests...');
+    });
   });
-});
 
