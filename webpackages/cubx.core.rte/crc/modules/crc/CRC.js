70cd34e9248535509e5e0aacfc84f25ff74f92df
Judit Ross <jtrs@incowia.com>
2017-02-22 14:44:52 +0100

    RTE-68 rte version 2.2.0

--- webpackages/cubx.core.rte/crc/modules/crc/CRC.js
+++ webpackages/cubx.core.rte/crc/modules/crc/CRC.js
@@ -2,7 +2,7 @@
 /**
  * Defines the CRC RequireJS Module.
  *
- * @version 2.2.0-SNAPSHOT
+ * @version 2.2.0
  * @module CRC_Module
  */
 window.cubx.amd.define([
@@ -28,7 +28,7 @@ window.cubx.amd.define([
        /* @type {string}
        /* @private
        */
-      this._version = '2.2.0-SNAPSHOT';
+      this._version = '2.2.0';
 
       /**
        * jQuery object containing only one element to which the crc should be appended
