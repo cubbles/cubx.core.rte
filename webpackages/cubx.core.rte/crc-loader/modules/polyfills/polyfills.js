d37a666103d83aa9d5668da0496e8f3598c2cd58
edwingamboa <edwingamboa355@gmail.com>
2018-04-30 12:23:00 -0500

    #14: Fix eslint errors

--- webpackages/cubx.core.rte/crc-loader/modules/polyfills/polyfills.js
+++ webpackages/cubx.core.rte/crc-loader/modules/polyfills/polyfills.js
@@ -123,15 +123,15 @@ cubx.amd.define([], function () {
       Element.prototype.matches = Element.prototype.msMatchesSelector ||
         Element.prototype.mozMatchesSelector ||
         Element.prototype.webkitMatchesSelector || function matches (selector) {
-          var element = this;
-          var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
-          var index = 0;
+        var element = this;
+        var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+        var index = 0;
 
-          while (elements[ index ] && elements[ index ] !== element) {
-            ++index;
-          }
-          return Boolean(elements[ index ]);
-        };
+        while (elements[ index ] && elements[ index ] !== element) {
+          ++index;
+        }
+        return Boolean(elements[ index ]);
+      };
     }
     if (typeof Element.prototype.closest !== 'function') {
       Element.prototype.closest = function closest (selector) {
