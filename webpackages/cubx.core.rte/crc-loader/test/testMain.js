38cacfa7582407a318c28a50bf25bff5e4076a66
edwingamboa <edwingamboa355@gmail.com>
2018-11-09 09:48:49 -0500

    #26: Add a comment to explain how and why karma.start was redefined

--- webpackages/cubx.core.rte/crc-loader/test/testMain.js
+++ webpackages/cubx.core.rte/crc-loader/test/testMain.js
@@ -6,6 +6,11 @@
 (function () {
   'use strict';
 
+  /**
+   * Make karma.start a empty function to prevent that karma starts before 
+   * requirejs is ready. In our case karma.start is equivalent to mocha.run, 
+   * which is called in the callback at the end of this script
+   */
   window.__karma__.start = function(){};
   
   // define all tests that should be performed (relative to test/index.html)
