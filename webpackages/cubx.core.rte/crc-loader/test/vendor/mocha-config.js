f929f6b40d4a9fa3e93421222ceee16133cfa3a1
edwingamboa <edwingamboa355@gmail.com>
2018-11-14 09:09:03 -0500

    26: Removw browser based tests

--- webpackages/cubx.core.rte/crc-loader/test/vendor/mocha-config.js
+++
@@ -1,7 +0,0 @@
-/* globals mocha, chai */
-/* eslint-disable no-unused-vars */
-mocha.setup({ui: 'bdd'});
-var assert = chai.assert;
-var expect = chai.expect;
-// Note that should has to be executed
-var should = chai.should();
